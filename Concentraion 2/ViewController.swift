9821874bd1989c86963a92842e9a1c2c3d3dbf3a
Arnav Singhania <arnavsinghania98@gmail.com>
2018-04-12 23:28:04 -0700

    Initial Commit

---
+++ Concentraion 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentraion 2
+//
+//  Created by ARNAV SINGHANIA on 4/12/18.
+//  Copyright © 2018 Singhania Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
